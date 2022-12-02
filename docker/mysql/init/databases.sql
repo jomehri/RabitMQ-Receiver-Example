b575aa65d9a732c733e24b4501e9c00d4cd3af7e
AliJomehri <ajomehri@gmail.com>
2022-12-02 14:57:15 +0330

    initial push

--- docker/mysql/init/databases.sql
+++ docker/mysql/init/databases.sql
@@ -1,6 +1,6 @@
 # create databases
-CREATE DATABASE IF NOT EXISTS `qst_db`;
-CREATE DATABASE IF NOT EXISTS `qst_testing_db`;
+CREATE DATABASE IF NOT EXISTS `alijomehri_db`;
+CREATE DATABASE IF NOT EXISTS `alijomehri_testing_db`;
 
 # create root user and grant rights
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
